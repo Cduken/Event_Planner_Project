4d6ffda38a8bab4bbcfb74fd80ec9f3602ebc547
Cduken <ernestojrcabarrubias@gmail.com>
2024-10-03 22:59:57 +0800

    added channel name

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
-export default {
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
     build: {
-      outDir: 'public/build',
-      manifest: true,
-      rollupOptions: {
-        input: 'resources/js/app.js',
-      },
+        outDir: 'public/build', // Ensure this is set
+        emptyOutDir: true,      // Optional: Clears the output directory on build
     },
-  };
-
+});
